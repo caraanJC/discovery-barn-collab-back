add01398fbc1ffb18d3549fc7e45b9a62b103863
Kaye <sbonite21@gmail.com>
2021-10-06 01:02:48 +0800

    fix: remove file_type property in tasklist

--- models/programs.js
+++ models/programs.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const programSchema = new schema({
-    name: String,
-    active_flag: Boolean,
-    task_list: [
-        {
-            title: String,
-            description: String,
-            deadline: { type: Date },
-            file_type: String,
-        },
-    ],
+	name: String,
+	active_flag: Boolean,
+	task_list: [
+		{
+			title: String,
+			description: String,
+			deadline: { type: Date }
+		}
+	]
 });
 
 module.exports = mongoose.model('programs', programSchema);
