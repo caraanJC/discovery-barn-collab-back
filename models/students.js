0f2b501c7ec2ee4086663a94c53e3052113662da
Kaye <sbonite21@gmail.com>
2021-10-06 14:46:00 +0800

    fix: add filename property in submissions model

--- models/students.js
+++ models/students.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const studentSchema = new schema({
-    first_name: String,
-    last_name: String,
-    age: Number,
-    program_id: { type: mongoose.Schema.Types.ObjectId, ref: 'programs' },
-    parent_id: { type: mongoose.Schema.Types.ObjectId, ref: 'parents' },
-    submissions: [
-        {
-            link: String,
-            task_id: String,
-            date_submitted: { type: Date, default: Date.now },
-        },
-    ],
+	first_name: String,
+	last_name: String,
+	age: Number,
+	program_id: { type: mongoose.Schema.Types.ObjectId, ref: 'programs' },
+	parent_id: { type: mongoose.Schema.Types.ObjectId, ref: 'parents' },
+	submissions: [
+		{
+			file_path: String,
+			file_name: String,
+			task_title: String,
+			date_submitted: { type: Date, default: Date.now }
+		}
+	]
 });
 
 module.exports = mongoose.model('students', studentSchema);
